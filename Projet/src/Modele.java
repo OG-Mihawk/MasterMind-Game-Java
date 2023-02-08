ca7a2ae364ed9ce5f08a34734b6be5022d6d85f0
Ashura <116311276+Ashura667@users.noreply.github.com>
2023-02-08 10:57:47 +0100

    Ajout de la classe rangée

--- Projet/src/Modele.java
+++ Projet/src/Modele.java
@@ -1,10 +1,12 @@
 import java.awt.*;
 
 public class Modele {
-    Color[] COULEURS = {Color.YELLOW, Color.GREEN,Color.BLUE,Color.MAGENTA,Color.RED,Color.ORANGE,Color.WHITE,Color.BLACK};
+    public static Color[] COULEURS = {Color.YELLOW, Color.GREEN,Color.BLUE,Color.MAGENTA,Color.RED,Color.ORANGE,Color.WHITE,Color.BLACK};
     int N_TENTATIVES;
-    int DIFFICULTE;
+    public static  int DIFFICULTE;
     enum Etat {
         EN_COURS,GAGNÉ,PERDU
     };
+
+
 }
